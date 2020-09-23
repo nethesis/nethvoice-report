bdad023790c8d5394e61ea1c8f87cdfbc6c64f32
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-09-23 15:41:00 +0200

    build. tasks: updated conf path

--- tasks/main.go
+++ tasks/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	// read and init configuration
-	ConfigFilePtr := flag.String("c", "/opt/nethvoice-report/conf.json", "Path to configuration file")
+	ConfigFilePtr := flag.String("c", "/opt/nethvoice-report/api/conf.json", "Path to configuration file")
 	flag.Parse()
 	configuration.Init(ConfigFilePtr)
 
