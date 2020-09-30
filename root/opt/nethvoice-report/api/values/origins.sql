c414dc1245525e78d700a2817c97971fdcf3888d
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-09-30 16:33:38 +0200

    values. update origins query

--- root/opt/nethvoice-report/api/values/origins.sql
+++ root/opt/nethvoice-report/api/values/origins.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT CONCAT(comune,",",provincia,",",regione) FROM report_queue_callers ORDER BY CONCAT(comune,",",provincia,",",regione);
+SELECT DISTINCT CONCAT(comune,",",provincia,",",regione) FROM report_queue_callers WHERE CONCAT(comune,",",provincia,",",regione) NOT LIKE "" ORDER BY CONCAT(comune,",",provincia,",",regione);
