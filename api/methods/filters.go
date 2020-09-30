3648c75de0b500e014d4d2e63b49d0b2ea72fa1f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-09-30 10:00:50 +0200

    copying. change header in each files

--- api/methods/filters.go
+++ api/methods/filters.go
@@ -2,20 +2,20 @@
  * Copyright (C) 2020 Nethesis S.r.l.
  * http://www.nethesis.it - info@nethesis.it
  *
- * This file is part of Icaro project.
+ * This file is part of NethVoice Report project.
  *
- * Icaro is free software: you can redistribute it and/or modify
+ * NethVoice Report is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License,
  * or any later version.
  *
- * Icaro is distributed in the hope that it will be useful,
+ * NethVoice Report is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Icaro.  If not, see COPYING.
+ * along with NethVoice Report.  If not, see COPYING.
  *
  * author: Edoardo Spadoni <edoardo.spadoni@nethesis.it>
  */
