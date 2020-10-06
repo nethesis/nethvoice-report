342d6838ec715eaa437330d1e193a6b4c3dce8bf
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-10-06 15:11:43 +0200

    Revert "build. added views action"
    This reverts commit 0d35a227b41fb16baf0a71bdf4d89f504aef0f77.

--- root/opt/nethvoice-report/api/views/view.sql
+++ root/opt/nethvoice-report/api/views/view.sql
@@ -1,1 +1,1 @@
-CREATE OR REPLACE VIEW performace_evase AS SELECT DATE_FORMAT(from_unixtime(timestamp_in),"%Y") AS period , qname, COUNT(id) FROM report_queue WHERE action='ANSWER' GROUP BY period,qname;
+CREATE VIEW performace_evase AS SELECT DATE_FORMAT(from_unixtime(timestamp_in),"%Y") AS period , qname, COUNT(id) FROM report_queue WHERE action='ANSWER' GROUP BY period,qname;
