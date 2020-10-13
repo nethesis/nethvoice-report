956bfdd0a6de042e5ce2845dffe420d5344890fe
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-10-13 12:13:56 +0200

    api. view & query. added graph load

---
+++ root/opt/nethvoice-report/api/views/graph_load.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS graph_load_total_year; 
+
+DROP TABLE IF EXISTS graph_load_total_month; 
+
+DROP TABLE IF EXISTS graph_load_total_week; 
+
+DROP TABLE IF EXISTS graph_load_total_day; 
+
+CREATE TABLE graph_load_total_year AS 
+  SELECT Date_format(From_unixtime(timestamp_in), "%Y") AS period, 
+         qname,
+         qdescr, 
+         Count(id)                                      AS total 
+  FROM   report_queue 
+  GROUP  BY qdescr, 
+            period 
+  ORDER  BY period,total desc; 
+
+  CREATE TABLE graph_load_total_month AS 
+  SELECT Date_format(From_unixtime(timestamp_in), "%Y-%m") AS period, 
+         qname,
+         qdescr, 
+         Count(id)                                      AS total 
+  FROM   report_queue 
+  GROUP  BY qdescr, 
+            period 
+  ORDER  BY period,total desc; 
+
+  CREATE TABLE graph_load_total_week AS 
+  SELECT Date_format(From_unixtime(timestamp_in), "%Y-%u") AS period, 
+         qname,
+         qdescr, 
+         Count(id)                                      AS total 
+  FROM   report_queue 
+  GROUP  BY qdescr, 
+            period 
+  ORDER  BY period,total desc; 
+
+  CREATE TABLE graph_load_total_day AS 
+  SELECT Date_format(From_unixtime(timestamp_in), "%Y-%m-%d") AS period, 
+         qname,
+         qdescr, 
+         Count(id)                                      AS total 
+  FROM   report_queue 
+  GROUP  BY qdescr, 
+            period 
+  ORDER  BY period,total desc; 
