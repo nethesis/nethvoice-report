f38a416c7ea10049c59d35ea7f342bc6fce8f26a
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-10-19 12:33:35 +0200

    api. query & view: added distribution hour

---
+++ root/opt/nethvoice-report/api/queries/distribution/hourly/3_table_failed.sql
@@ -0,0 +1,21 @@
+{{ if gt (len .Queues) 0 }}
+SELECT
+        period AS period£{{ .Time.Group }}Date,
+        qname,
+        qdescr,
+        time_{{ .Time.Division  }} AS time£hourDate,
+        total AS total£num
+FROM    distribution_hour_failed_{{ .Time.Group }}_{{ .Time.Division  }}
+WHERE   time_{{ .Time.Division  }} >= '{{ ExtractSettings "StartHour" }}' AND time_{{ .Time.Division  }} <= '{{ ExtractSettings "EndHour" }}'
+        {{ if and .Time.Interval.Start .Time.Interval.End }}
+            AND period >= "{{ .Time.Interval.Start }}"
+            AND period <= "{{ .Time.Interval.End }}"
+        {{ end }}
+        {{ if gt (len .Queues) 0 }}
+            AND qname in ({{ ExtractStrings .Queues }})
+        {{ end }}
+GROUP BY period, time_{{ .Time.Division }},qdescr
+ORDER BY period, qdescr,time_{{ .Time.Division }}
+{{ else }}
+SELECT "queues field is required" AS "!message";
+{{ end }}
