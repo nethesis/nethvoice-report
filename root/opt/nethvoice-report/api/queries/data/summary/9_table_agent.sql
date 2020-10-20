368e4350c530ef686d050210ac14d49ff2227576
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-10-20 17:32:57 +0200

    api. query: update data agent query

--- root/opt/nethvoice-report/api/queries/data/summary/9_table_agent.sql
+++ root/opt/nethvoice-report/api/queries/data/summary/9_table_agent.sql
@@ -6,7 +6,7 @@ SELECT period  AS period£{{ .Time.Group }}Date,
        minbill AS duration$min_duration£seconds, 
        avgbill AS duration$avg_duration£seconds, 
        maxbill AS duration$max_duration£seconds 
-FROM   data_summary_agent_{{ .Time.Group }};
+FROM   data_summary_agent_{{ .Time.Group }}
 WHERE   TRUE
         {{ if and .Time.Interval.Start .Time.Interval.End }}
             AND period >= "{{ .Time.Interval.Start }}"
