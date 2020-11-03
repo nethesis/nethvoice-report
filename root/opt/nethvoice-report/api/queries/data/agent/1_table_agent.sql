6123b0af024daf6f911187c7d487d45f663221d1
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-11-03 10:41:48 +0100

    api. query: added collapsed cols in data by agent

--- root/opt/nethvoice-report/api/queries/data/agent/1_table_agent.sql
+++ root/opt/nethvoice-report/api/queries/data/agent/1_table_agent.sql
@@ -1,20 +1,20 @@
 SELECT
-    period AS period£{{ .Time.Group }}Date,
+    period AS `period£{{ .Time.Group }}Date`,
     agent,
     qname,
     qdescr,
-    logon as activity$logonDays£days,
-    login as activity$login£seconds,
-    pause as activity$pause£seconds,
-    login - pause as activity$effective£seconds,
-    answered as load$answered£num,
-    unanswered as load$unAnswered£num,
-    totcall as load$totCall£seconds,
-    round(answered / ((login - pause) / 3600), 2) as load$callOnHour£num,
-    round(totcall / (login - pause), 2) as load$occupation£percent,
-    min_duration as duration$min_duration£seconds,
-    max_duration as duration$max_duration£seconds,
-    avg_duration as duration$avg_duration£seconds
+    logon as `activity$logonDays£days`,
+    login as `activity$login£seconds#hide`,
+    pause as `activity$pause£seconds#hide`,
+    login - pause as `activity$effective£seconds#hide`,
+    totcall as `load$totCall£seconds`,
+    answered as `load$answered£num#hide`,
+    unanswered as `load$unAnswered£num#hide`,
+    round(answered / ((login - pause) / 3600), 2) as `load$callOnHour£num#hide`,
+    round(totcall / (login - pause), 2) as `load$occupation£percent#hide`,
+    min_duration as `duration$min_duration£seconds`,
+    max_duration as `duration$max_duration£seconds`,
+    avg_duration as `duration$avg_duration£seconds`
 FROM data_agent_{{ .Time.Group }}
 WHERE TRUE
     {{ if and .Time.Interval.Start .Time.Interval.End }}
