24dc2b6590ef3b0eb302e4ec5328d98bea577082
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-11-13 16:02:14 +0100

    tasks. completed cdr command with query

--- root/opt/nethvoice-report/api/templates/cdr_year.sql
+++ root/opt/nethvoice-report/api/templates/cdr_year.sql
@@ -1,4 +1,51 @@
 CREATE TABLE IF NOT EXISTS `cdr_{{ YearMap .Year }}` AS
-SELECT *
-FROM cdr
-WHERE date_format(calldate, '%Y') = {{ .Year }}
+SELECT *,
+       IF(Substring_index(Substring_index(channel, '-', 1), '/', -1) IN
+             (SELECT channelid
+                                                                         FROM
+             asterisk.trunks), "IN", IF(Substring_index(Substring_index(
+                                                        dstchannel, '-'
+                                                        , 1), '/', -1) IN (
+                                        SELECT
+                                                                      channelid
+                                        FROM
+       asterisk.trunks), "OUT", "LOCAL"))  AS type,
+       Group_concat(disposition, "")       AS dispositions,
+       (SELECT Group_concat(disposition)
+        FROM   cdr
+        WHERE  linkedid = c.uniqueid
+               AND uniqueid != c.uniqueid) AS dispositions_children,
+       Group_concat(lastapp, "")           AS lastapps
+FROM   cdr c
+WHERE  uniqueid = linkedid
+       AND date_format(calldate, "%Y") = "{{ .Year }}"
+GROUP  BY linkedid,
+          peeraccount
+ORDER  BY calldate;
+
+ALTER TABLE `cdr_{{ YearMap .Year }}` ADD UNIQUE (calldate,uniqueid,dstchannel);
+
+INSERT IGNORE INTO `cdr_{{ YearMap .Year }}`
+SELECT *,
+       IF(Substring_index(Substring_index(channel, '-', 1), '/', -1) IN
+             (SELECT channelid
+                                                                         FROM
+             asterisk.trunks), "IN", IF(Substring_index(Substring_index(
+                                                        dstchannel, '-'
+                                                        , 1), '/', -1) IN (
+                                        SELECT
+                                                                      channelid
+                                        FROM
+       asterisk.trunks), "OUT", "LOCAL"))  AS type,
+       Group_concat(disposition, "")       AS dispositions,
+       (SELECT Group_concat(disposition)
+        FROM   cdr
+        WHERE  linkedid = c.uniqueid
+               AND uniqueid != c.uniqueid) AS dispositions_children,
+       Group_concat(lastapp, "")           AS lastapps
+FROM   cdr c
+WHERE  uniqueid = linkedid
+       AND date_format(calldate, "%Y-%m-%d") = date_format(NOW() - INTERVAL 1 DAY, "%Y-%m-%d")
+GROUP  BY linkedid,
+          peeraccount
+ORDER  BY calldate;
