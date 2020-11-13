34a18465bd7fa9c0a4073d78b2ff3d2fce362965
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-11-13 16:02:14 +0100

    tasks. completed cdr command with query

---
+++ root/opt/nethvoice-report/api/templates/cdr_month.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `cdr_{{ YearMap .Year }}-{{ MonthMap .Month }}` AS
+SELECT *
+FROM `cdr_{{ YearMap .Year }}`
+WHERE date_format(calldate, "%Y-%m") = "{{ .Year }}-{{ MonthMap .Month }}";
+
+ALTER TABLE `cdr_{{ YearMap .Year }}` ADD UNIQUE (calldate,uniqueid,dstchannel);
+
+INSERT IGNORE INTO `cdr_{{ YearMap .Year }}-{{ MonthMap .Month }}`
+SELECT *
+FROM `cdr_{{ YearMap .Year }}`
+WHERE AND date_format(calldate, "%Y-%m-%d") = date_format(NOW() - INTERVAL 1 DAY, "%Y-%m-%d");
