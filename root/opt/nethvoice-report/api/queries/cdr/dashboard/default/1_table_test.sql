71cd12b916e4bd9b7f2f63575b5b0e9854f29a6e
Andrea Leardini <andrea.leardini@nethesis.it>
2020-11-26 10:21:16 +0100

    show call time in test query

--- root/opt/nethvoice-report/api/queries/cdr/dashboard/default/1_table_test.sql
+++ root/opt/nethvoice-report/api/queries/cdr/dashboard/default/1_table_test.sql
@@ -1,5 +1,9 @@
+-- //// remove test query
 select
-    src, dst, type
+    src,
+    dst,
+    type,
+    DATE_FORMAT(calldate, '%Y-%m-%d %H:%i:%s') AS time
     from `<CDR_TABLE>`
     where
         calldate >= "{{ .Time.Interval.Start }}"
