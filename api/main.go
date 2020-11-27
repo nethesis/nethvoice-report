1b7520287db7632d845496a1353961d7d78e1d28
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-11-27 10:57:58 +0100

    api. added new enpoint /details

--- api/main.go
+++ api/main.go
@@ -80,6 +80,11 @@ func main() {
 			filters.GET("", methods.GetDefaultFilter)
 		}
 
+		details := api.Group("/details/:linkedid")
+		{
+			details.GET("", methods.GetCallDetails)
+		}
+
 		phonebook := api.Group("/phonebook")
 		{
 			phonebook.GET("", methods.GetPhonebook)
