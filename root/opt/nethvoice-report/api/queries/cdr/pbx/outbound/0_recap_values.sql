ef188ac73020628068a2630add88c6168f1bfc79
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-12-03 16:45:12 +0100

    api. recap: include call type

--- root/opt/nethvoice-report/api/queries/cdr/pbx/outbound/0_recap_values.sql
+++ root/opt/nethvoice-report/api/queries/cdr/pbx/outbound/0_recap_values.sql
@@ -1,4 +1,6 @@
-SELECT Count(*)                                                 AS "total£num", 
+SELECT type,
+       call_type,	
+       Count(*)                                                 AS "total£num", 
        Sum(IF(dispositions REGEXP 'ANSWERED$', 1, 0))           AS "answered£num", 
        Sum(IF(dispositions REGEXP 'NO ANSWER$', 1, 0))          AS "noAnswer£num", 
        Sum(IF(dispositions REGEXP 'BUSY$', 1, 0))               AS "busy£num", 
@@ -6,8 +8,10 @@ SELECT Count(*)                                                 AS "total£num",
                  OR dispositions REGEXP 'CONGESTION$' ), 1, 0)) AS "failed£num",
        Sum(duration)						AS "totalDuration£seconds",	
        Avg(duration)                                            AS "avgDuration£seconds",
-       Sum(cost)						AS "totalCost£currency",
-       Avg(cost)                                                AS "avgCost£currency", 
+       Sum(COALESCE(cost,0))					AS "totalCost£currency",
+       Avg(COALESCE(cost,0))                                    AS "avgCost£currency", 
        Sum(duration) - Sum(billsec)				AS "totalWait£seconds",
        Avg(duration) - Avg(billsec)                             AS "avgWait£seconds" 
-FROM   `<CDR_TABLE>`; 
+FROM   `<CDR_TABLE>`
+GROUP BY type, call_type
+ORDER BY type, call_type;
