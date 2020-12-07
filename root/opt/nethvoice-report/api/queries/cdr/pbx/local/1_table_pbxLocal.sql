8d0240d2fb0d6d3faefc2e82e96da988c4630450
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-12-07 15:33:24 +0100

    api. cdr: completed pbx section queries

--- root/opt/nethvoice-report/api/queries/cdr/pbx/local/1_table_pbxLocal.sql
+++ root/opt/nethvoice-report/api/queries/cdr/pbx/local/1_table_pbxLocal.sql
@@ -1,5 +1,20 @@
-SELECT  *
-FROM    `<CDR_TABLE>`
-WHERE   calldate >= "{{ .Time.Interval.Start }}"
-        AND calldate <= "{{ .Time.Interval.End }}"
-        AND type = "LOCAL"
+SELECT	*
+FROM	`<CDR_TABLE>`
+WHERE	calldate >= "{{ .Time.Interval.Start }}"
+	AND calldate <= "{{ .Time.Interval.End }}"
+	AND type = "LOCAL"
+	{{ if gt (len .Sources) 0 }}
+	  AND src IN ({{ ExtractStrings .Sources }})
+	{{ end }}
+	{{ if gt (len .Destinations) 0 }}
+	  AND dst IN ({{ ExtractStrings .Destinations }})
+	{{ end }}
+	{{ if .CallType }}
+	  AND dispositions REGEXP "{{ .CallType }}$"
+	{{ end }}
+	{{ if .Duration }}
+	  AND duration <= {{ .Duration }}
+	{{ end }}
+	{{ if gt (len .Trunks) 0 }}
+	  AND channel LIKE "%{{ .Trunks }}%"
+	{{ end }}
