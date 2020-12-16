3e081ae888fbb4909717420185d6349ddc06f718
Andrea Leardini <andrea.leardini@nethesis.it>
2020-12-16 16:17:26 +0100

    ui. enhanced call details and CDR query

--- root/opt/nethvoice-report/api/queries/cdr/dashboard/default/1_table_test.sql
+++ root/opt/nethvoice-report/api/queries/cdr/dashboard/default/1_table_test.sql
@@ -3,9 +3,9 @@ select
     linkedid,
     src AS src£phoneNumber,
     dst AS dst£phoneNumber,
-    type,
+    type AS call_type£label,
     DATE_FORMAT(calldate, '%Y-%m-%d %H:%i:%s') AS time,
-    SUBSTRING_INDEX(dispositions, ',',- 1) AS result, -- get last disposition
+    SUBSTRING_INDEX(dispositions, ',',- 1) AS result£label, -- get last disposition
     billsec,
     cost
     -- //// clid, dcontext, channel, dstchannel, lastapp, lastdata, duration, disposition, amaflags, accountcode, uniqueid, userfield, did, recordingfile, cnum, cnam, outbound_cnum, outbound_cnam, dst_cnam, peeraccount, sequence, ccompany, dst_ccompany, lastapps, dcontexts, call_type
