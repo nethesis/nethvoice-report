be42fc91cf395c6d3157fde248f461595566d913
Andrea Leardini <andrea.leardini@nethesis.it>
2020-12-17 12:41:16 +0100

    ui. rename call_type to callType

--- api/models/filter.go
+++ api/models/filter.go
@@ -33,7 +33,7 @@ type Filter struct {
 	DIDs             []string `json:"dids"`
 	Sources          []string `json:"sources"`
 	Destinations     []string `json:"destinations"`
-	CallType         []string `json:"call_type"`
+	CallType         []string `json:"callType"`
 	Duration         string   `json:"duration"`
 	CallDestinations []string `json:"callDestinations"`
 	Patterns         []string `json:"patterns"`
