0b66bc4abadabcdf631ac41f7185bbde081774a6
Andrea Leardini <andrea.leardini@nethesis.it>
2020-12-18 09:52:56 +0100

    ui. fix "Clear all filters"

--- ui/src/components/Filters.vue
+++ ui/src/components/Filters.vue
@@ -418,6 +418,7 @@
               :searchFields="['title', 'description', 'phoneNumber']"
               @input="onSourcesInput"
               @select="onSourcesSelect"
+              ref="sources"
             />
           </sui-form-field>
           <!-- cdr: call destination / callee -->
@@ -430,6 +431,7 @@
               :searchFields="['title', 'description', 'phoneNumber']"
               @input="onDestinationsInput"
               @select="onDestinationsSelect"
+              ref="destinations"
             />
           </sui-form-field>
           <!-- cdr: call type -->
@@ -1829,12 +1831,15 @@ export default {
       this.filter.results = [];
       this.filter.choices = [];
       this.filter.destinations = {};
+      this.$refs.destinations.$data.query = "";
       this.filter.origins = [];
       this.filter.caller = "";
       this.filter.contactName = "";
       this.filter.callType = [];
       this.filter.duration = {};
+      this.$refs.duration.$data.query = "";
       this.filter.sources = {};
+      this.$refs.sources.$data.query = "";
       this.filter.trunks = [];
       this.filter.users = [];
       this.filter.callDestinations = [];
