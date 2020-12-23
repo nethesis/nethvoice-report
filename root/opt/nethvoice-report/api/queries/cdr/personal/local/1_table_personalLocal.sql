4feed53240dc0616dcba540b6a8198c667c16f60
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-12-23 10:18:57 +0100

    api. cdr: define fields for each call type

--- root/opt/nethvoice-report/api/queries/cdr/personal/local/1_table_personalLocal.sql
+++ root/opt/nethvoice-report/api/queries/cdr/personal/local/1_table_personalLocal.sql
@@ -1,4 +1,12 @@
-SELECT	*
+SELECT	
+    linkedid,
+    src AS src£phoneNumber,
+    dst AS dst£phoneNumber,
+    type AS call_type£label,
+    DATE_FORMAT(calldate, '%Y-%m-%d %H:%i:%s') AS time£hourDate,
+    SUBSTRING_INDEX(dispositions, ',',- 1) AS result£label, -- get last disposition
+    duration AS billsec£seconds,
+    cost AS cost£currency
 FROM	`<CDR_TABLE>`
 WHERE	calldate >= "{{ .Time.Interval.Start }}"
 	AND calldate <= "{{ .Time.Interval.End }}"
