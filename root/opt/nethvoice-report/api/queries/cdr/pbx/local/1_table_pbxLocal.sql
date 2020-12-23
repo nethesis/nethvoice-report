8da41cc7628df0a0ff262dde3d51f0349dc44da0
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-12-23 10:18:57 +0100

    api. cdr: define fields for each call type

--- root/opt/nethvoice-report/api/queries/cdr/pbx/local/1_table_pbxLocal.sql
+++ root/opt/nethvoice-report/api/queries/cdr/pbx/local/1_table_pbxLocal.sql
@@ -1,13 +1,12 @@
 SELECT
-	linkedid,
+    linkedid,
     src AS src£phoneNumber,
     dst AS dst£phoneNumber,
     type AS call_type£label,
-    DATE_FORMAT(calldate, '%Y-%m-%d %H:%i:%s') AS time,
+    DATE_FORMAT(calldate, '%Y-%m-%d %H:%i:%s') AS time£hourDate,
     SUBSTRING_INDEX(dispositions, ',',- 1) AS result£label, -- get last disposition
-    billsec,
-    cost
-    -- //// clid, dcontext, channel, dstchannel, lastapp, lastdata, duration, disposition, amaflags, accountcode, uniqueid, userfield, did, recordingfile, cnum, cnam, outbound_cnum, outbound_cnam, dst_cnam, peeraccount, sequence, ccompany, dst_ccompany, lastapps, dcontexts, call_type
+    duration AS billsec£seconds,
+    cost AS cost£currency
 FROM	`<CDR_TABLE>`
 WHERE	calldate >= "{{ .Time.Interval.Start }}"
 	AND calldate <= "{{ .Time.Interval.End }}"
