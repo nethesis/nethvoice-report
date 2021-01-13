82063b79e6ce65c5b6bb9c3e775111aab39c55e8
Andrea Leardini <andrea.leardini@nethesis.it>
2021-01-13 18:03:28 +0100

    queries & ui. final columns for cdr & call details

--- root/opt/nethvoice-report/api/queries/cdr/personal/outbound/1_table_personalOutbound.sql
+++ root/opt/nethvoice-report/api/queries/cdr/personal/outbound/1_table_personalOutbound.sql
@@ -5,7 +5,6 @@ SELECT
     dst AS dst£phoneNumber,
     type AS call_type£label,
     SUBSTRING_INDEX(dispositions, ',',- 1) AS result£label, -- get last disposition
-	  did,
     duration AS totalDuration£seconds,
     billsec AS billsec£seconds,
     cost AS cost£currency
