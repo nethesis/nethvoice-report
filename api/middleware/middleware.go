e0cc61a4551d4ddf8547ede9df70a09546730a2f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2021-01-28 10:04:28 +0100

    api. extend jwt expiration to 1 week

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -62,7 +62,7 @@ func InitJWT() *jwt.GinJWTMiddleware {
 	authMiddleware, errDefine := jwt.New(&jwt.GinJWTMiddleware{
 		Realm:       "queue-report",
 		Key:         []byte(configuration.Config.Secret),
-		Timeout:     time.Hour * 12,
+		Timeout:     time.Hour * 168,
 		MaxRefresh:  time.Hour * 24 * 30, // a month
 		IdentityKey: identityKey,
 		Authenticator: func(c *gin.Context) (interface{}, error) {
