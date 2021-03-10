8850477e81927ce0d6d7b698c15c78fc4860f24e
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2021-03-10 09:37:25 +0100

    api. queries: add time on data_lost query

--- root/opt/nethvoice-report/api/queries/queue/data/lost_call/1_table_lostCall.sql
+++ root/opt/nethvoice-report/api/queries/queue/data/lost_call/1_table_lostCall.sql
@@ -12,8 +12,8 @@ FROM
     data_lost
 WHERE   TRUE
         {{ if and .Time.Interval.Start .Time.Interval.End }}
-            AND period >= "{{ .Time.Interval.Start }}"
-            AND period <= "{{ .Time.Interval.End }}"
+            AND period >= "{{ .Time.Interval.Start }} 00:00:00"
+            AND period <= "{{ .Time.Interval.End }} 23:59:59"
         {{ end }}
         {{ if gt (len .Phones) 0 }}
             AND cid in ({{ ExtractPhones .Phones true }})
