c300e5ef380f2f81e523359509b4b970f92f2124
Sebastian <sebastian.besel@nethesis.it>
2021-09-30 11:42:24 +0200

    api & ui. add total duration to call done by agent

--- root/opt/nethvoice-report/api/queries/queue/data/summary/9_table_callDoneByAgent.sql
+++ root/opt/nethvoice-report/api/queries/queue/data/summary/9_table_callDoneByAgent.sql
@@ -5,7 +5,8 @@ SELECT period  AS period£{{ .Time.Group }}Date,
        uniqCallees AS uniqCallees£num, 
        minbill AS duration$min_duration£seconds, 
        avgbill AS duration$avg_duration£seconds, 
-       maxbill AS duration$max_duration£seconds 
+       maxbill AS duration$max_duration£seconds,
+       totalbill AS duration$total_duration£seconds
 FROM   data_summary_agent_{{ .Time.Group }}
 WHERE   TRUE
         {{ if and .Time.Interval.Start .Time.Interval.End }}
