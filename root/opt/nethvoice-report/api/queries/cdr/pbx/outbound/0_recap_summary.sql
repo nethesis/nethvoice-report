dd50fa594e645b64658ca931618942380058fb6d
Sebastian <sebastian.besel@nethesis.it>
2021-10-01 17:04:02 +0200

    api & ui. add total effective duration to cdr recap summary

--- root/opt/nethvoice-report/api/queries/cdr/pbx/outbound/0_recap_summary.sql
+++ root/opt/nethvoice-report/api/queries/cdr/pbx/outbound/0_recap_summary.sql
@@ -1,4 +1,4 @@
-<CDR_SELECT: type,call_type,sum(total£num) AS "total£num",sum(answered£num) AS "answered£num",sum(noAnswer£num) AS "noAnswer£num",sum(busy£num) AS "busy£num",sum(failed£num) AS "failed£num",sum(totalDuration£seconds) AS "totalDuration£seconds",avg(avgDuration£seconds) AS "avgDuration£seconds",sum(totalCost£currency) AS "totalCost£currency",avg(avgCost£currency) AS "avgCost£currency",sum(totalWait£seconds) AS "totalWait£seconds",avg(avgWait£seconds) AS "avgWait£seconds">
+<CDR_SELECT: type,call_type,sum(total£num) AS "total£num",sum(answered£num) AS "answered£num",sum(noAnswer£num) AS "noAnswer£num",sum(busy£num) AS "busy£num",sum(failed£num) AS "failed£num",sum(totalDuration£seconds) AS "totalDuration£seconds",avg(avgDuration£seconds) AS "avgDuration£seconds",sum(totalCost£currency) AS "totalCost£currency",avg(avgCost£currency) AS "avgCost£currency",sum(totalWait£seconds) AS "totalWait£seconds",avg(avgWait£seconds) AS "avgWait£seconds", sum(totalEffectiveDuration£seconds) AS "totalEffectiveDuration£seconds">
 SELECT type,
        call_type,	
        Count(*)                                                 AS "total£num", 
@@ -12,7 +12,8 @@ SELECT type,
        Sum(COALESCE(cost,0))					AS "totalCost£currency",
        Avg(COALESCE(cost,0))                                    AS "avgCost£currency", 
        Sum(duration) - Sum(billsec)				AS "totalWait£seconds",
-       Avg(duration) - Avg(billsec)                             AS "avgWait£seconds" 
+       Avg(duration) - Avg(billsec)                             AS "avgWait£seconds" , 
+       Sum(duration) - (Sum(duration) - Sum(billsec))           AS "totalEffectiveDuration£seconds" 
 FROM   `<CDR_TABLE>`
 WHERE	calldate >= "{{ .Time.Interval.Start }}"
 	AND calldate <= "{{ .Time.Interval.End }}"
