f5356bb6776532bef90b0324dc6b9b06422ce320
Stefano <stefano.fancello@nethesis.it>
2021-12-03 14:34:43 +0100

    Fix trunk name comparison for trunks with dash in cdr report (#145)
    nethesis/dev#6098

--- root/opt/nethvoice-report/api/templates/cdr_cost_update.sql
+++ root/opt/nethvoice-report/api/templates/cdr_cost_update.sql
@@ -2,4 +2,4 @@ UPDATE `{{ .Table }}`
 SET    	cost = billsec * {{ .Cost }} / 60
 WHERE  	type = "OUT"
 	AND call_type = "{{ .Destination }}"
-	AND Substring_index(Substring_index(dstchannel,'-', 1), '/', -1) = "{{ .Trunk }}"
+	AND Substring_index(SUBSTRING(dstchannel,1,LENGTH(dstchannel)-LOCATE('-',REVERSE(dstchannel))), '/', -1) = "{{ .Trunk }}"
