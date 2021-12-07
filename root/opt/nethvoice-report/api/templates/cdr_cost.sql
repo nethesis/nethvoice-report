e2b47be5fa08d2d9864a803a808bd414af9c88ad
Stefano <stefano.fancello@nethesis.it>
2021-12-07 17:01:23 +0100

    Revert "Fix trunk name comparison for trunks with dash in cdr report (#145)" (#148)
    This reverts commit f5356bb6776532bef90b0324dc6b9b06422ce320.
    nethesis/dev#6098

--- root/opt/nethvoice-report/api/templates/cdr_cost.sql
+++ root/opt/nethvoice-report/api/templates/cdr_cost.sql
@@ -1,7 +1,10 @@
 UPDATE `{{ .Table }}`
 SET    cost = billsec * (SELECT cost / 60
                          FROM   cost_details
-                         WHERE  destination = call_type
-				AND channelid = Substring_index(SUBSTRING(dstchannel,1,LENGTH(dstchannel)-LOCATE('-',REVERSE(dstchannel))), '/', -1)
-			)
-WHERE  cost IS NULL AND type = "OUT";
+                         WHERE  destination = call_type 
+                                AND channelid = Substring_index(Substring_index( 
+                                                            dstchannel, 
+                                                            '-', 1 
+                                                            ), '/', -1 
+                                            )) 
+WHERE  cost IS NULL AND type = "OUT"; 
