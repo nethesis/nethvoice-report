71f240005ad79acfb2ba67b464552ef060f74cb8
Stefano <stefano.fancello@nethesis.it>
2022-04-22 10:01:23 +0200

    Fix recall_hour.sql
    Put in recall_hour table only the missed calls that has been recalled

--- root/opt/nethvoice-report/api/views/recall_hour.sql
+++ root/opt/nethvoice-report/api/views/recall_hour.sql
@@ -7,10 +7,11 @@ SELECT
        qdescr,
        Date_format(From_unixtime(timestamp_in), "%H:00") AS HOUR,
        Count(id) AS num,
-       Avg(IF(data4 > 0, data4, null)) AS avg_recall_time
+       Avg(data4) AS avg_recall_time
 FROM
        report_queue
 WHERE action IN ('ABANDON', 'EXITWITHTIMEOUT', 'EXITWITHKEY', 'EXITEMPTY', 'FULL', 'JOINEMPTY', 'JOINUNAVAIL')
+AND data4 > 0
 GROUP BY
        qdescr,
        period,
